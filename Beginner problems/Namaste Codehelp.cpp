c4d3b53398407bcaed6c89d5be3ad8623bd1005c
SHIKHARCHATURVEDI19 <shikhar7936@gmail.com>
2025-12-02 22:05:44 +0530

    Initial commit

---
+++ Beginner problems/Namaste Codehelp.cpp
@@ -0,0 +1,32 @@
+
+
+/*
+
+Example
+Here is what the output should look like:
+
+
+@@Namaste Codehelp@@
+As shown, the text "Namaste Codehelp" is enclosed with "@@" at both the start and the end, exactly matching the required format.
+
+Example 1:
+
+Input:
+
+name = Codehelp
+
+Output:
+
+Namaste Codehelp
+Explanation:The program should output the exact phrase as shown without any extra spaces or new lines.
+
+*/
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"@@Namaste Codehelp@@";
+return 0;
+}
