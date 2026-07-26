0e0cced34314641bb8d76d4f3bfa68e5e44c6e4b
SHIKHARCHATURVEDI19 <shikhar7936@gmail.com>
2026-07-26 23:25:45 +0530

    lc

---
+++ July month leetcode/26lc.cpp
@@ -0,0 +1,46 @@
+628. Maximum Product of Three Numbers
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 6
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 24
+Example 3:
+
+Input: nums = [-1,-2,-3]
+Output: -6
+ 
+
+Constraints:
+
+3 <= nums.length <= 104
+-1000 <= nums[i] <= 1000
+
+
+
+
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        int option1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+        int option2 = nums[0] * nums[1] * nums[n - 1];
+
+        return max(option1, option2);
+    }
+};
